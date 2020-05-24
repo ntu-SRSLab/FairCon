0991bec1669ef6c4bb7a2f96bc989b7ff0a4fba5
liu <liuye5@live.com>
2020-05-24 22:06:34 +0800

    Add script to environment

--- getcmake.sh
+++
@@ -1,9 +0,0 @@
-version=3.16
-build=5
-mkdir ~/temp
-cd ~/temp
-wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
-tar -xzvf cmake-$version.$build.tar.gz
-cd cmake-$version.$build/
-./bootstrap
-make -j$(nproc)
