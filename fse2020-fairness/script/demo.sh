e648980d26767a985924db95fd277c9ce851c5df
liu <liu>
2020-05-25 12:52:19 +0800

    update demo.sh

--- fse2020-fairness/script/demo.sh
+++ fse2020-fairness/script/demo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "fairness testing on CryptoRomeAuction.sol"
-faircon  -symexe-main  ../smartcontract/experiment/auction/CryptoRomeAuction.sol
+faircon  -symexe-main  /home/fairness/smartcontract/experiment/auction/CryptoRomeAuction.sol
